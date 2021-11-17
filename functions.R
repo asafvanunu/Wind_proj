cff0e491333948855edb498aa6ce6db542a495d6
Asaf <asafyu@post.bgu.ac.il>
2021-11-17 14:27:51 +0200

    Implemented FEVD

--- functions.R
+++ functions.R
@@ -16,6 +16,11 @@ LoadIMSData <- function(data_path, station_name) {
     # Force all to be numeric (Some will become NA)
     # ims_wind[,3:13] = apply(ims_wind[3:13], 2,
     #                         function(x) as.numeric(as.character(x)))
+    ims_wind <- filter(ims_wind, year(date_time) >= 2000)
+    ims_wind$WD_UpperGust_degrees[ims_wind$WD_UpperGust_degrees <0] <- NA
+    ims_wind$WD_UpperGust_degrees[ims_wind$WD_UpperGust_degrees  == 360] <- 0
+    ims_wind$compass <- as.integer(ims_wind$WD_UpperGust_degrees/45)+1
+    
     ims_wind$station = station_name
     return(ims_wind[,2:ncol(ims_wind)])
   })
@@ -37,10 +42,27 @@ PlotWindrose <- function (station_data) {
                              facet = wind_df$station_name, 
                              col_pal = "YlOrRd", n_col = 3,
                              n_speeds = 7,
-                             legend_title = "מהירות רוח [מטר/שנייה]",
+                             legend_title = "Wind speed m/s",
                              strip.text = element_text(size=12, face="bold"),
                              plot.title = element_text(size=14, face="bold", hjust = 0.5))
   
   print(rose_plot)
 }
 
+extreme_value = function (IMS_merged,stn) {
+    max_gust_year = IMS_merged %>%
+      group_by(year = floor_date(date_time, "year")) %>%
+      summarize(mean_WS_ms = mean(WS_ms, na.rm=T),
+              max_WS_ms=max(WS_ms,na.rm=T),
+              mean_Gust_ms = mean(WS_UpperGust_ms,na.rm=T),
+              max_gust_ms=max(WS_UpperGust_ms,na.rm=T))
+    
+    fit = fevd(max_gust_year$max_gust_ms, data=max_gust_year,
+               type = "GEV"
+               )
+    
+    return_level = return.level(fit)
+    return_level
+    plot(fit, main = stn )
+    
+}
--- wind_script.R
+++ wind_script.R
@@ -1,61 +1,9 @@
-install.packages("tidyverse")
-install.packages("data.table")
-library(tidyverse)
-library(data.table)
-library(lubridate)
-
-setwd("C:\\Users\\asaf_rs\\Desktop\\wind\\Wind_proj\\stations\\IMS\\Nativ\\try")
-
-my_files <- list.files(pattern = "*.csv")
-
-
-Nativ = lapply(my_files, function(i){
-x = read.csv(i)
-x})
-
-Nativ = do.call("rbind.data.frame", Nativ)
-
-## delete station name
-Nativ = Nativ[,-1]
-
-Nativ = as_tibble(Nativ)
-
-##set the date column into date class
-Nativ$Date = as.Date(Nativ$Date, "%d/%m/%Y")
-##set the time column
-Nativ$Hour_LST = as.ITime(Nativ$Hour_LST)
-## set numeric columns
-Nativ[,3:13] = apply(Nativ[3:13],2,function(x) as.numeric(as.character(x)))
-
-
-##Micha part
-ims_wind_csv = "Nativ.csv"
-ims_wind = read.csv(ims_wind_csv, row.names = 1)
-# Read in with hour as LST where GMT is 2 hours behind
-ims_wind$date_time = as_datetime(paste(ims_wind$Date, ims_wind$Hour_LST),
-                                 tz = "Etc/GMT-2")
-# Convert to UTC ("Greenwich Mean Time")
-ims_wind$date_time = as_datetime(ims_wind$date_time, tz = "UTC")
-# Check conversion:
-head(ims_wind[,c("Date", "Hour_LST", "date_time")])
-tail(ims_wind[,c("Date", "Hour_LST", "date_time")])
-
-
-# Data structure
-
-str(ims_wind)
-
-#change column order
-
-ims_wind <- ims_wind[, c(1,2,14,3,4,5,6,7,8,9,10,11,12,13)]
-
-write.csv(ims_wind, file = "Nativ_station.csv")
-
 #' Required packages
 #' Check for installed packages, and install if needed
 pkg_list <- c("tidyverse", "data.table", "readr", # fast reading of CSV
               "lubridate",                        # date processing
-              "clifro")                           # wind rose
+              "clifro",
+              "extRemes")                           # wind rose
 installed_packages <- pkg_list %in% rownames(installed.packages())
 if (any(installed_packages == FALSE)) {
   install.packages(pkg_list[!installed_packages])
@@ -82,30 +30,15 @@ station_data_list <- lapply(stations, function(stn){
 })
 station_data <- do.call(rbind, station_data_list)
 
+station_data_list <- lapply(stations, function(stn){
+  station_df <- station_data[station==stn,]
+  extreme_value(station_df, stn)
+})
 PlotWindrose(station_data)
 
-# Nativ = lapply(my_files, function(i){
-# x = read.csv(i)
-# x})
-# 
-# Nativ = do.call("rbind.data.frame", Nativ)
-# 
-# ## delete station name
-# Nativ = Nativ[,-1]
-# 
-# Nativ = as_tibble(Nativ)
-# 
-# ##set the date column into date class
-# Nativ$Date = as.Date(Nativ$Date, "%d/%m/%Y")
-# ##set the time column
-# Nativ$Hour_LST = as.ITime(Nativ$Hour_LST)
-# ## set numeric columns
-# Nativ[,3:13] = apply(Nativ[3:13],2,function(x) as.numeric(as.character(x)))
-#
 
-str(IMS_merged)
-# write.csv(Nativ, file = "Nativ.csv")
-# 
+
+
 
 
 
